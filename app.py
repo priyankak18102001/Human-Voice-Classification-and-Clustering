ba3316d47cd37b4ba804b812c909159935ef7150
priyankak18102001 <priyankakumawat227@gmail.com>
2025-11-23 20:16:37 +0530

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -8,15 +8,6 @@ from pathlib import Path
 import librosa, tempfile
 import pickle
 
-BASE_DIR = Path(__file__).parent  # folder where app.py lives
-FEATURE_PATH = BASE_DIR / "models" / "selected_features_20.pkl"
-
-if not FEATURE_PATH.exists():
-    st.error("Feature list not found. Make sure models/selected_features_20.pkl exists.")
-else:
-    with open(FEATURE_PATH, "rb") as f:
-        selected_features = pickle.load(f)
-
 
 # Dimensionality reduction
 from sklearn.decomposition import PCA
@@ -274,6 +265,7 @@ elif page == "Prediction & Clustering":
 
 
               
+
 
 
 
